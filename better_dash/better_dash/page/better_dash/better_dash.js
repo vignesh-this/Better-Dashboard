5e2f3b84b5f9c5a6e13e4b09ce713cb2b1c3ff03
vignesh <vignesh.this@gmail.com>
2019-08-22 14:45:15 +0530

    changes

--- better_dash/better_dash/page/better_dash/better_dash.js
+++ better_dash/better_dash/page/better_dash/better_dash.js
@@ -4,209 +4,302 @@ frappe.pages['better-dash'].on_page_load = function (wrapper) {
 		title: 'A Better Dashboard',
 		single_column: true
 	});
-	new better_dash(page);
-
+	new frappe.views.BetterDashboard(page);
 }
 frappe.pages['better-dash'].refresh = function (wrapper) {
-
 }
-class better_dash {
-	constructor(page) {
+
+
+frappe.views.BetterDashboard = Class.extend({
+	init: function(page) {
 		this.page = page;
-		var date_field = undefined;
-		this.mainfunc = function () {
-			var me = this;
-			frappe.call({
-				method: "better_dash.better_dash.page.better_dash.better_dash.get_all_data",
-				args: {
-					"date": date_field
-				},
-				callback: function (r) {
-					console.log(r.message)
-					page.body.html(frappe.render_template("dash_layout", { "data": r.message }));
-					$("input[type='checkbox']").change(function () {
-						console.log($(this).parent().parent().parent().attr("data-name"))
-					});
-					$('[data-toggle="tooltip"]').tooltip();
-					this.date_field = frappe.ui.form.make_control({
-						df: {
-							fieldtype: 'Date',
-							label: 'Date',
-							fieldname: 'date_field',
-							onchange: () => {
-								console.log(this.date_field.get_value());
-								date_field = this.date_field.get_value();
-								me.mainfunc();
+		this.data = undefined;
+		this.args = undefined;
+		this.page.show_menu();
+		this.add_menu_buttons();
+		this.get_data();
+		this.render_filters_template();
+		this.make_filters();
+		this.render_base_template();
+		this.make_context_menu();
+		this.set_linked_docs_action();
+		this.set_secondary_action();
+	},
+	get_data: function () {
+		var me = this;
+		frappe.call({
+			method: "better_dash.better_dash.page.better_dash.better_dash.get_all_data",
+			args: me.args,
+			async: false,
+			callback: function (r) {
+				me.data = r.message;
+			}
+		});
+	},
+	render_filters_template: function () {
+		var me = this;
+		me.page.body.prepend(frappe.render_template("dash_filters", { "data": me.data }));		
+	},
+	render_base_template: function () {
+		var me = this;
+		console.log(me.data);	
+		$(me.page.body).find("#dash-body-main").remove();
+		me.page.body.append(frappe.render_template("dash_layout", { "data": me.data }));
+		me.list_actions();
+	},
+	list_actions: function () {
+		var me = this;
+		$(".list-action").click(function () {
+			var id = $(this).attr('id');
+			var action = id.substr(3);
+			id = id.substring(0,2);
+			var id_mapper = {
+							"so": {"div": ".gaps.sales-order", "doctype": "Sales Order"},
+							"mr": {"div": ".gaps.material-request", "doctype": "Material Request"},
+							"po": {"div": ".gaps.purchase-order", "doctype": "Purchase Order"},
+							"pr": {"div": ".gaps.purchase-receipt", "doctype": "Purchase Receipt"},
+							"dn": {"div": ".gaps.delivery-note", "doctype": "Delivery Note"},
+							"si": {"div": ".gaps.sales-invoice", "doctype": "Sales Invoice"}
 							}
+
+			var selected_docs = [];
+			var selected_doctype = id_mapper[id].doctype;
+			$(id_mapper[id].div).find("input:checked").each(function() {
+				selected_docs.push($(this).attr('data-name'));
+			});
+			console.log(selected_docs);
+			if (["submit", "cancel", "update"].includes(action)) {
+				frappe.confirm(__("Submit Checked Documents ??"), function() {
+					frappe.call({
+						method: "frappe.desk.doctype.bulk_update.bulk_update.submit_cancel_or_update_docs",
+						args:{
+							"doctype": selected_doctype,
+							"action": action,
+							"docnames": selected_docs
 						},
-						parent: $(page.body.html).find('#date-filter'),
-						render_input: true
-					});
-					if (date_field) {
-						$(this.date_field.$input).val(date_field);
-					}
-					this.supplier_field = frappe.ui.form.make_control({
-						df: {
-							fieldtype: 'Link',
-							label: 'Supplier',
-							fieldname: 'supplier_field',
-							options: 'Supplier',
-							onchange: () => {
-								console.log(this.supplier_field.get_value());
+						callback: function (r) {
+							let failed = r.message;
+							if (!failed) failed = [];
+			
+							if (failed.length && !r._server_messages) {
+								frappe.throw(__('Cannot {0} {1}', [action, failed.map(f => f.bold()).join(', ')]));
 							}
-						},
-						parent: $(page.body.html).find('#supplier-filter'),
-						render_input: true
-					});
-					this.customer_field = frappe.ui.form.make_control({
-						df: {
-							fieldtype: 'Link',
-							label: 'Customer',
-							options: 'Customer',
-							fieldname: 'customer_field',
-							onchange: () => {
-								console.log(this.customer_field.get_value());
+							if (failed.length < docnames.length) {
+								frappe.utils.play_sound(action);
+								if (done) done();
 							}
-						},
-						parent: $(page.body.html).find('#customer-filter'),
-						render_input: true
-					});
-					this.medley_order_id_field = frappe.ui.form.make_control({
-						df: {
-							fieldtype: 'Link',
-							label: 'Medley Order Id',
-							options: 'Sales Order',
-							fieldname: 'medley_order_id_field',
-							onchange: () => {
-								console.log(this.medley_order_id_field.get_value());
-							}
-						},
-						parent: $(page.body.html).find('#medley-oi-filter'),
-						render_input: true
-					});
-					this.medley_master_order_id_field = frappe.ui.form.make_control({
-						df: {
-							fieldtype: 'Link',
-							label: 'Medley Master Order Id',
-							options: 'Purchase Receipt',
-							fieldname: 'medley_master_id',
-							onchange: () => {
-								console.log(this.medley_master_order_id_field.get_value());
-							}
-						},
-						parent: $(page.body.html).find('#medley-masterid-filter'),
-						render_input: true
-					});
-					this.company = frappe.ui.form.make_control({
-						df: {
-							fieldtype: 'Link',
-							label: 'Company',
-							options: 'Company',
-							fieldname: 'company',
-							onchange: () => {
-								console.log(this.company.get_value());
-							}
-						},
-						parent: $(page.body.html).find('#company-filter'),
-						render_input: true
-					});
-					var $contextMenu = $("#contextMenu");
+						}
+					});		
+				});
+			} else {
+				frappe.throw("Sorry !!!");
+			}			
 
-					$(".gaps .tab_drop").contextmenu(function (e) {
-						var doctype = $(this).attr("data-doctype");
-						var docname = $(this).attr("data-name");
-						$(contextMenu).attr('data-doctype', doctype);
-						$(contextMenu).attr('data-name', docname);
-
-						$contextMenu.css({
-							display: "block",
-							left: e.pageX,
-							top: e.pageY - 130,
-							background: "#FFFFFF",
-							border: "1px solid #dddddd",
-							padding: ".5rem .25rem",
-						});
-						return false;
-
-
-					});
-
-					// this.reset_button = frappe.ui.form.make_control({
-					// 	df: {
-					// 		fieldtype: 'Button',
-					// 		label: 'Reset',
-					// 		fieldname: 'reset',
-					// 		click: () => {
-					// 			console.log(this.reset_button);
-					// 		}
-					// 	},
-					// 	parent: $(page.body.html).find('#reset-filters'),
-					// 	render_input: true
-					// });
-
-					$('html').click(function () {
-						$contextMenu.hide();
-					});
-
-					$("#highlight_linked").click(function () {
-						$(".panel-heading").css("background-color", "#f7fafc");
-						var doctype = $(this).parent().parent().parent().attr("data-doctype");
-						var docname = $(this).parent().parent().parent().attr("data-name");
-						$("[data-doctype='" + doctype + "'][data-name='" + docname + "']").find(".panel-heading")[0].scrollIntoView();
-						$("[data-doctype='" + doctype + "'][data-name='" + docname + "']").find(".panel-heading").css("background-color", "#6ebfa9");
-						frappe.call({
-							method: "frappe.desk.form.linked_with.get_linked_doctypes",
-							args: {
-								doctype: doctype
-							},
-							callback: (r) => {
-								frappe.call({
-									method: "frappe.desk.form.linked_with.get_linked_docs",
-									args: {
-										doctype: doctype,
-										name: docname,
-										linkinfo: r.message,
-									},
-									callback: (r) => {
-										$.each(r.message, function (key, value) {
-
-											for (let index = 0; index < r.message[key].length; index++) {
-												$("[data-doctype='" + key + "'][data-name='" + r.message[key][index].name + "']").find(".panel-heading").css("background-color", "#6ebfa9");
-												$("[data-doctype='" + key + "'][data-name='" + r.message[key][index].name + "']")[0].scrollIntoView();
-												if (key == "Purchase Order") {
-
-													frappe.call({
-														method: "frappe.client.get_list",
-														args: {
-															doctype: "Purchase Receipt",
-															filters: [
-																["purchase_order_no", "=", r.message[key][index].name]
-															],
-															fields: ["name"]
-														},
-														callback: function (r) {
-															for (let lo = 0; lo < r.message.length; lo++) {
-																const element = r.message[lo];
-																$("[data-doctype='Purchase Receipt'][data-name='" + element.name + "']").find(".panel-heading").css("background-color", "#6ebfa9");
-																$("[data-doctype='Purchase Receipt'][data-name='" + element.name + "']")[0].scrollIntoView();
-															}
-														}
-													});
-
-												}
-
-											}
-
-										});
-									}
-								});
-							}
-						});
-					});
+		});
+	},
+	make_filters: function () {
+		var me = this;
+		page = me.page;
+		this.date_field = frappe.ui.form.make_control({
+			df: {
+				fieldtype: 'Date',
+				label: 'Date',
+				fieldname: 'date_field',
+				onchange: () => {
+					console.log(this.date_field.get_value());
+					me.get_filtered_data();
 				}
-			})
+			},
+			parent: $(page.body.html).find('#date-filter'),
+			render_input: true
+		});
+		this.supplier_field = frappe.ui.form.make_control({
+			df: {
+				fieldtype: 'Link',
+				label: 'Supplier',
+				fieldname: 'supplier_field',
+				options: 'Supplier',
+				onchange: () => {
+					console.log(this.supplier_field.get_value());
+				}
+			},
+			parent: $(page.body.html).find('#supplier-filter'),
+			render_input: true
+		});
+		this.customer_field = frappe.ui.form.make_control({
+			df: {
+				fieldtype: 'Link',
+				label: 'Customer',
+				options: 'Customer',
+				fieldname: 'customer_field',
+				onchange: () => {
+					console.log(this.customer_field.get_value());
+				}
+			},
+			parent: $(page.body.html).find('#customer-filter'),
+			render_input: true
+		});
+		this.medley_order_id_field = frappe.ui.form.make_control({
+			df: {
+				fieldtype: 'Link',
+				label: 'Medley Order Id',
+				options: 'Sales Order',
+				fieldname: 'medley_order_id_field',
+				onchange: () => {
+					console.log(this.medley_order_id_field.get_value());
+				} 
+			},
+			parent: $(page.body.html).find('#medley-oi-filter'),
+			render_input: true
+		});
+		this.medley_master_order_id_field = frappe.ui.form.make_control({
+			df: {
+				fieldtype: 'Link',
+				label: 'Medley Master Order Id',
+				options: 'Purchase Receipt',
+				fieldname: 'medley_master_id',
+				onchange: () => {
+					console.log(this.medley_master_order_id_field.get_value());
+				}
+			},
+			parent: $(page.body.html).find('#medley-masterid-filter'),
+			render_input: true
+		});
+		this.company = frappe.ui.form.make_control({
+			df: {
+				fieldtype: 'Link',
+				label: 'Company',
+				options: 'Company',
+				fieldname: 'company',
+				onchange: () => {
+					console.log(this.company.get_value());
+				}
+			},
+			parent: $(page.body.html).find('#company-filter'),
+			render_input: true
+		});
+	},
+	make_context_menu: function () {
+		var me = this;
+		var $contextMenu = $("#contextMenu");
+		$(".gaps .tab_drop").contextmenu(function (e) {
+			var doctype = $(this).attr("data-doctype");
+			var docname = $(this).attr("data-name");
+			$(contextMenu).attr('data-doctype', doctype);
+			$(contextMenu).attr('data-name', docname);
 
-		}
-		this.mainfunc();
+			$contextMenu.css({
+				display: "block",
+				left: e.pageX,
+				top: e.pageY - 130,
+				background: "#FFFFFF"
+			});
+			return false;
+		});
+
+		$('html').click(function () {
+			$contextMenu.hide();
+		});		
+	},
+	set_linked_docs_action: function () {
+		var me = this;
+		$("#highlight_linked").click(function () {
+			$(".panel-heading").css("background-color", "#f7fafc");
+			var doctype = $(this).parent().parent().parent().attr("data-doctype");
+			var docname = $(this).parent().parent().parent().attr("data-name");
+			$("[data-doctype='" + doctype + "'][data-name='" + docname + "']").find(".panel-heading")[0].scrollIntoView();
+			$("[data-doctype='" + doctype + "'][data-name='" + docname + "']").find(".panel-heading").css("background-color", "#6ebfa9");
+			me.get_linked_data(doctype, docname);					
+		});
+	},
+	get_linked_data: function (doctype, docname) {
+
+		frappe.call({
+			method: "frappe.desk.form.linked_with.get_linked_doctypes",
+			args: {
+				doctype: doctype
+			},
+			callback: (r) => {
+				frappe.call({
+					method: "frappe.desk.form.linked_with.get_linked_docs",
+					args: {
+						doctype: doctype,
+						name: docname,
+						linkinfo: r.message,
+					},
+					callback: (r) => {
+						console.log(r.message)
+						$.each(r.message, function (key, value) {
+
+							for (let index = 0; index < r.message[key].length; index++) {
+								$("[data-doctype='" + key + "'][data-name='" + r.message[key][index].name + "']").find(".panel-heading").css("background-color", "#6ebfa9");
+								$("[data-doctype='" + key + "'][data-name='" + r.message[key][index].name + "']")[0].scrollIntoView();
+								if (key == "Purchase Order") {
+
+									frappe.call({
+										method: "frappe.client.get_list",
+										args: {
+											doctype: "Purchase Receipt",
+											filters: [
+												["purchase_order_no", "=", r.message[key][index].name]
+											],
+											fields: ["name"]
+										},
+										callback: function (r) {
+											for (let lo = 0; lo < r.message.length; lo++) {
+												const element = r.message[lo];
+												$("[data-doctype='Purchase Receipt'][data-name='" + element.name + "']").find(".panel-heading").css("background-color", "#6ebfa9");
+												$("[data-doctype='Purchase Receipt'][data-name='" + element.name + "']")[0].scrollIntoView();
+											}
+										}
+									});
+
+								}
+
+							}
+
+						});
+					}
+				});
+			}
+		});		
+		
+	},
+	get_filtered_data: function () {
+		var me = this;
+		me.args = {"date": me.date_field.get_value()}
+		me.get_data();
+		me.render_base_template();
+		me.make_context_menu();
+		me.set_linked_docs_action();
+	},
+	add_menu_buttons: function () {
+		var me = this;
+		me.page.add_menu_item(__("Reset Filters"), function() {
+			me.reset_filters();
+		});
+		me.page.add_menu_item(__("Reset All Data"), function() {
+			me.reset_data();
+		});
+	},
+	reset_filters: function () {
+		var me = this;	
+		$("#filter-tab").remove();
+		me.render_filters_template();
+		me.make_filters();
+	},
+	reset_data: function () {
+		var me = this;
+		me.args = undefined;
+		me.get_data();
+		me.render_base_template();
+		me.make_context_menu();
+		me.set_linked_docs_action();		
+	},
+	set_secondary_action() {
+		var me = this;
+		me.page.set_secondary_action(__("Refresh"), function() {
+			me.reset_data();
+		}, null, __("Please Wait..."))
 	}
-}
+});	
